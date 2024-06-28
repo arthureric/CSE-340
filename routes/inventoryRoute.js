e167d1d615604e2f3ada3800ae2016ac4e7c145f
arthureric <art21005@byui.edu>
2024-06-28 13:27:58 +0000

    error fix

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,9 +1,29 @@
 // Needed Resources 
+const classValidate = require('../utilities/inventory-validation')
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/index");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/detail/:invId", utilities.handleErrors(invController.showInventoryDetail));
+router.get("/", utilities.handleErrors(invController.managementView));
+router.get("/add-classification", invController.addClassificationView);
+router.get("/add-inventory", invController.addInventoryView);
+
+router.post(
+    "/add-classification",
+    classValidate.classificationRules(),
+    classValidate.checkclassData,
+    utilities.handleErrors(invController.addClassification)
+);
+
+router.post(
+    "/add-inventory", 
+    classValidate.addInventoryRules(),
+    classValidate.checkInvData,
+    utilities.handleErrors(invController.addInventory)
+);
 
 module.exports = router; // Exporting the router for use in other modules
