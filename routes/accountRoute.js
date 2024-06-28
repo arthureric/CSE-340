659301e3b83602a8cd6e6b4a5f00762495841059
arthureric <art21005@byui.edu>
2024-06-28 12:12:33 +0000

    part of week 4

---
+++ routes/accountRoute.js
@@ -0,0 +1,19 @@
+const regValidate = require('../utilities/account-validation')
+const express = require("express")
+const router = new express.Router() 
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities/index");
+
+router.get('/login', utilities.handleErrors(accountController.buildLogin));
+
+router.get('/register', utilities.handleErrors(accountController.buildRegister));
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,33 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/index")
+const errorRoute = require("./routes/errorRoute")
+const errorHandler = require("./routes/errorRoute")
+const session = require("express-session")
+const pool = require('./database/')
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -25,13 +52,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-
 // Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
-
 // Inventory routes
-app.use("/inv", inventoryRoute)
-
+app.use("/inv", utilities.handleErrors(inventoryRoute))
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
