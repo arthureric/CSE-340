5c3fe40d17dcc314bd7e54c3c2c1fbbb7e3b8367
arthureric <art21005@byui.edu>
2024-07-05 05:04:50 +0000

    Team Activity Added

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,21 +1,71 @@
-const regValidate = require('../utilities/account-validation')
-const express = require('express');
-const router = express.Router();
-const accountController = require("../controllers/accountController")
-const utilities = require("../utilities/index");
+// Needed Resources 
+const logValidate = require('../utilities/login-validation')
+const regValidate = require('../utilities/registration-validation')
+const express = require("express") // Brings express into scope.
+const router = new express.Router() // Using express we create a new router object.
+const accountController = require("../controllers/accountController") // Brings the invController into scope.
+const Util = require('../utilities/');
 
-router.get('/login', utilities.handleErrors(accountController.buildLogin));
+/* ***************************
+ *  GET
+ * ************************** */
+// Deliver Default Account View
+router.get(
+  "/", 
+  Util.checkLogin, 
+  Util.handleErrors(accountController.buildAccountManagementHomeView)
+);
 
-router.get('/register', utilities.handleErrors(accountController.buildRegister));
+// Deliver Login View
+router.get("/login", accountController.buildLoginView);
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Deliver Edit Account View
+router.get(
+  "/logout",
+  Util.handleErrors(accountController.logoutAccount),
+)
+
+// Deliver Register View
+router.get("/register", accountController.buildRegisterView);
+
+// Deliver Edit Account View
+router.get("/edit/:account_id", accountController.buildEditAccountView);
+
+
+
+/* ***************************
+ *  POST
+ * ************************** */
+// Process the login data
+router.post(
+  "/login",
+  logValidate.loginRules(),
+  logValidate.checkLoginData,
+  Util.handleErrors(accountController.processAccountLogin)
+)
 
 // Process the registration data
 router.post(
-    "/register",
-    regValidate.registationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
-  )
+  "/register",
+  regValidate.regRules(),
+  regValidate.checkRegData,
+  Util.handleErrors(accountController.registerNewAccount)
+)
 
-module.exports = router;
+// Process the account update
+router.post(
+  "/update-account",
+  logValidate.updateAccountRules(),
+  logValidate.checkEditAccountData,
+  Util.handleErrors(accountController.processAccountUpdate)
+)
+
+// Process the account password change
+router.post(
+  "/change-password",
+  logValidate.changePasswordRules(),
+  logValidate.checkEditAccountData,
+  Util.handleErrors(accountController.processAccountPasswordUpdate)
+)
+
+module.exports = router; // exports the router objects to be used elsewhere in the project.
