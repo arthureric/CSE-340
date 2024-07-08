f3abb06e142e66fa05f8032e52a1581d6a1cfbd8
arthureric <art21005@byui.edu>
2024-07-08 07:59:39 +0000

    week4

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,6 +1,7 @@
 const invModel = require("../models/inventory-model")
 const utilities = require("../utilities/")
 
+
 const invCont = {}
 
 /* ***************************
@@ -8,14 +9,10 @@ const invCont = {}
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId
-  // Get inventory items by ID
   const data = await invModel.getInventoryByClassificationId(classification_id)
-  // Build  grid
-  const grid = await utilities.buildClassificationGrid(data);
-  // navigation menu
+  const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
-  const className = data[0].classification_name;
-  // Render the classification
+  const className = data[0].classification_name
   res.render("./inventory/classification", {
     title: className + " vehicles",
     nav,
@@ -35,5 +32,99 @@ invCont.buildByInvId = async function (req, res, next) {
     grid,
   })
 }
+
+invCont.buildManagement = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/management", {
+    title: "Vehicle Management",
+    nav,
+    errors: null
+  })
+}
+
+invCont.buildAddClassification = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/add-classification", {
+    title: "Add New Classification",
+    nav,
+    errors: null
+  })
+}
+
+invCont.buildAddInventory = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  const classificationList = await utilities.buildClassificationList();
+  res.render("./inventory/add-inventory", {
+    title: "Add New Vehicle",
+    nav,
+    classificationList,
+    errors: null
+  })
+}
+
+/* ****************************************
+*  Process Classification
+* *************************************** */
+invCont.manageClassification = async function (req, res) {
+  const { classification_name } = req.body
+  const managementResult = await invModel.addClassification(
+   classification_name
+  )
+  let nav = await utilities.getNav()
+
+  if (managementResult) {
+    req.flash(
+      "notice",
+      `The newcar classification was succesfully aded.`
+    )
+    res.status(201).render("./inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      errors: null
+    });
+  } else {
+    req.flash("notice", "Provide a correct clasification name.")
+    res.status(501).render("./inventory/add-classification", {
+      title: "Add New Classification",
+      nav,
+      errors: null
+    })
+  }
+}
+
+/* ****************************************
+*  Process new vehicle
+* *************************************** */
+invCont.manageInventory = async function (req, res) {
+  let nav = await utilities.getNav()
+  const { classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color } = req.body
+
+
+  const vehicleResult = await invModel.addNewVehicle(
+    classification_id, inv_make, inv_model, inv_description, inv_image, inv_thumbnail, inv_price, inv_year, inv_miles, inv_color
+  )
+
+  if (vehicleResult) {
+    req.flash(
+      "notice",
+      `The new vehicle was succesfully aded.`
+    )
+    res.status(201).render("./inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      errors: null
+    });
+  } else {
+    req.flash("notice", "Sorry, the vehicle could not be added. Please try again.")
+    res.status(501).render("./inventory/add-inventory", {
+      title: "Add New Vehicle",
+      nav,
+      errors: null
+    })
+  }
+}
+
+
+
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,12 +3,33 @@ const express = require("express")
 const router = new express.Router() // Using express we create a new router object.
 const invController = require("../controllers/invController")
 const utilities = require("../utilities")
+const invValidate = require('../utilities/inventory-validation')
 
 // Route to build invetory by classification view
+router.get("/", utilities.handleErrors(invController.buildManagement))
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
 
 // Route to build car view
 router.get("/detail/:invId", utilities.handleErrors(invController.buildByInvId))
 router.get("/detail/", utilities.handleErrors(invController.buildByInvId))
+
+router.get("/add-classification/", utilities.handleErrors(invController.buildAddClassification))
+router.get("/add-inventory/", utilities.handleErrors(invController.buildAddInventory))
+
+// Process the classification data
+router.post(
+    "/add-classification",
+    invValidate.classificationRules(),
+    invValidate.checkClassData,
+    utilities.handleErrors(invController.manageClassification)
+  )
+
+// Process the classification data
+router.post(
+  "/add-inventory",
+  invValidate.inventoryRules(),
+  invValidate.checkInventoryData,
+  utilities.handleErrors(invController.manageInventory)
+)
 
 module.exports = router; // Exporting the router for use in other modules
